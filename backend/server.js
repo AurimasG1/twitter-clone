79f30db3f1ca7b52c5480f969864c31d302f6a0b
AurimasG1 <112963534+AurimasG1@users.noreply.github.com>
2025-09-06 18:30:12 +0300

    4 - prepared our app for deployment + (.env=production)

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
@@ -18,6 +19,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '2mb' })); // to parse req.body DoS attack denial of service
 // limit shouldn't be too high to prevent DoS
@@ -29,6 +31,21 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+	// Serve frontend static files
+	app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+	// ALL other requests server frontend
+	app.get('/{*any}', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+	});
+} else {
+	// in Development, just show a simple message on root
+	app.get('/', (req, res) => {
+		res.send('API is running...');
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`server is up and running on port ${PORT}`);
